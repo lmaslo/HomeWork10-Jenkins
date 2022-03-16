f5b417fd0a2fcda633a245d3d5fed09a17808540
Lena Masloboishchikova <perepechkinaEV@yandex.ru>
2022-03-16 14:35:33 +0300

    add simple tests

---
+++ src/test/java/test/SkippedTests.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+    @Test
+    @Disabled
+    void test00 () {
+        assertTrue (false);
+    }
+
+    @Test
+    @Disabled("with some reason")
+    void test01 () {
+        assertTrue (false);
+    }
+
+
+}
